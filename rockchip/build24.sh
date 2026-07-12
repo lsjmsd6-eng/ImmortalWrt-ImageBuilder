d96672b39d7789c4da2a7899a4b907a13d074453
lsjmsd6-eng <lsjmsd6@gmail.com>
2026-07-12 22:39:55 +0800

    Update build24.sh

--- rockchip/build24.sh
+++ rockchip/build24.sh
@@ -54,7 +54,6 @@ cat repositories.conf
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
 PACKAGES="$PACKAGES openssh-sftp-server"
-PACKAGES="$PACKAGES -cpufreq"
 
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
